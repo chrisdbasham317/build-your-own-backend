cd06007a45bec83e2dac5083c9099a48d2dfbf1d
Chris Basham <chrisdbasham0317@gmail.com>
2019-11-21 10:57:07 -0700

    Fix syntax error on get endpoint urls

--- server.js
+++ server.js
@@ -8,17 +8,17 @@ const database = require('knex')(configuration);
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'DND Class Info';
 
-app.get('api/v1/classes', (request, response) => {
+app.get('/api/v1/classes', (request, response) => {
   database('classes').select()
     .then((classes) => {
       response.status(200).json(classes)
     })
     .catch(error => {
       response.status(500).json({ error: 'Unable to retrieve classes' });
-    })
-})
+    });
+});
 
-app.get('api/v1/subclasses', (request, response) => {
+app.get('/api/v1/subclasses', (request, response) => {
   database('subclasses').select()
     .then((subclasses) => {
       response.status(200).json(subclasses)
@@ -27,8 +27,8 @@ app.get('api/v1/subclasses', (request, response) => {
       response.status(500).json({
         error: 'Unable to retrieve subclasses'
       });
-    })
-})
+    });
+});
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on localhost:${app.get('port')}`)
