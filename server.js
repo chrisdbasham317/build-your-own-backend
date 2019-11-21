bda94b1b45d6404597805e61218e1329fc717ca6
Chris Basham <chrisdbasham0317@gmail.com>
2019-11-21 11:03:27 -0700

    Add get request by id for classes table

--- server.js
+++ server.js
@@ -10,13 +10,24 @@ app.locals.title = 'DND Class Info';
 
 app.get('/api/v1/classes', (request, response) => {
   database('classes').select()
-    .then((classes) => {
+    .then(classes => {
       response.status(200).json(classes)
     })
     .catch(error => {
       response.status(500).json({ error: 'Unable to retrieve classes' });
     });
 });
+
+app.get('/api/v1/classes/:id', (request, response) => {
+  database('classes').where('id', request.params.id).select()
+    .then(classes => {
+      if (classes.length) {
+        response.status(200).json(classes);
+      } else {
+        response.status(404).json({ error: `No class with ID: ${request.params.id} exists` });
+      }
+    })
+})
 
 app.get('/api/v1/subclasses', (request, response) => {
   database('subclasses').select()
